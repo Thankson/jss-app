269f22ad34cd759b5456c882c651b143da773c93
Qingwen Zeng <zengqingwen616@163.com>
2022-11-22 22:36:16 +0800

    use next/image with storybook

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -1,6 +1,8 @@
 import { Text, RichText, Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
+import Image from 'next/image';
 import styles from './ContentBlock.module.scss';
+import ScLogo from 'assets/images/sitecore_logo.svg';
 
 type ContentBlockProps = ComponentProps & {
   fields: {
@@ -19,6 +21,17 @@ const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => {
     <div className="contentBlock">
       <div className={styles.name}>Steven</div>
       <div className="text-3xl font-bold underline">Hello world!</div>
+      <div>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className="logo" /> */}
+          <Image src={ScLogo} alt="A stack of colorful cans" />
+        </a>
+      </div>
       <Text tag="h2" className="contentTitle" field={fields.heading} />
       <RichText className="contentDescription" field={fields.content} />
     </div>
